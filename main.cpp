0bb0a7fdc6fdec6a09aa62ccad6d3ba92bdaad66
chihara-shigou <k024g0039@g.neec.ac.jp>
2025-04-16 15:46:53 +0900

    ログの実装

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 ﻿#include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
+#include <windows.h>
+#include <string>
+
+
+//ログを出す関数
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
 
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
@@ -63,7 +101,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ShowWindow(hwnd, SW_SHOW);
 
 	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello,DIrectX!\n");
+	Log("Hello,DIrectX!\n");
+	Log(ConvertString(std::format(
+		L"width:{},{}\n", kClientWidth,
+		kClientHeight
+	)));
 
 	MSG msg{};
 	//ウィンドウの×ボタンが押されるまでループ
