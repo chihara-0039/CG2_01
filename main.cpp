26d23571e2a4229d2784e8516c58d7f507894049
chihara-shigou <k024g0039@g.neec.ac.jp>
2025-05-16 20:15:02 +0900

    POSITIONを修正
    問題：三角形の描画が出来てない？
    処理が続くので危険

--- main.cpp
+++ main.cpp
@@ -501,7 +501,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	//InputLayout
 	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
-	inputElementDescs[0].SemanticName = "POSITIO";
+	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
