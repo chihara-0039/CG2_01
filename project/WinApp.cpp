47533a0f07c10fd4dc7c689caa6e43eef256f256
Shigou Chihara <k024g0039@g.neec.ac.jp>
2026-03-04 12:29:25 +0900

    a

--- project/WinApp.cpp
+++ project/WinApp.cpp
@@ -17,7 +17,7 @@ void WinApp::Initialize() {
 
     // --- メンバ変数名 wc_ に統一 ---
     wc_.lpfnWndProc = WindowProc;
-    wc_.lpszClassName = L"CG2WindowClass";
+    wc_.lpszClassName = "CG2WindowClass";
     wc_.hInstance = GetModuleHandle(nullptr);
     wc_.hCursor = LoadCursor(nullptr, IDC_ARROW);
     RegisterClass(&wc_);
@@ -28,7 +28,7 @@ void WinApp::Initialize() {
     // --- メンバ変数名 hwnd_ に統一 ---
     hwnd_ = CreateWindow(
         wc_.lpszClassName,
-        L"CG2",
+        "CG2",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
