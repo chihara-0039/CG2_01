9e10dd7dfe5bcc69e4227a3726876bd7e1020d7b
SakuraTaiki <k024g0010@g.neec.ac.jp>
2026-04-03 16:43:37 +0900

    同期後実行完了　エラーも直したよー

--- project/Game/Scene/MyGame.cpp
+++ project/Game/Scene/MyGame.cpp
@@ -821,7 +821,7 @@ void MyGame::Draw() {
 
             dxCommon->PostDraw();
             return; // ←ここ重要！他の描画しない
-
+        }
         // --- 天球の描画（背景として最初に描く） ---
         if (skydomeObject_) {
             skydomeObject_->SetCamera(camera->GetViewMatrix(), camera->GetProjectionMatrix());
