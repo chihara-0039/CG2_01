31d3082b1459fdf0dc0e698969952f1b7f3c06b2
Shigou Chihara <k024g0039@g.neec.ac.jp>
2026-07-15 10:57:33 +0900

    シーン管理修正

--- project/Game/Scene/BaseScene.h
+++ project/Game/Scene/BaseScene.h
@@ -3,13 +3,19 @@
 #include "SceneUpdateContext.h"
 
 class MyGame;
+class SceneManager;
 
 class BaseScene {
 public:
     virtual ~BaseScene() = default;
 
+    void SetSceneManager(SceneManager* sceneManager) { sceneManager_ = sceneManager; }
+
     virtual void Initialize(MyGame& game) = 0;
     virtual void Update(MyGame& game, const SceneUpdateContext& context) = 0;
     virtual void Draw(MyGame& game) = 0;
     virtual void Finalize(MyGame& game) = 0;
+
+protected:
+    SceneManager* sceneManager_ = nullptr;
 };
